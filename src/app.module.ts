fadd76e1b655c34a5447782fe33c222d4987a3bc
Oleksii <alexey.bortnytskyi@gmail.com>
2025-04-18 15:52:35 +0200

    feature(src) - removed unused controllers & service's, created new user <Model, Controller, Module & Service>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
+/* eslint-disable node/no-process-env */
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { SequelizeModule } from '@nestjs/sequelize'
 
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { User } from './users/users.model'
+import { UsersModule } from './users/users.module'
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [User],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
 })
-
 export class AppModule { }
