6bec93e31dce9baf30e362d0cbc6aadd10ca0a51
Oleksii <alexey.bortnytskyi@gmail.com>
2025-05-19 23:33:24 +0200

    refactor(app) - removed unused components & packages, installed cross-env

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { UsersModule } from './users/users.module'
   providers: [],
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
